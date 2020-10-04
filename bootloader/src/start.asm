ef115a4c555cf012d7f7e3f04eaa531a8d2a876e
Patrik M. Rosenström <patrik.millvik@gmail.com>
2020-10-04 20:51:37 +0200

    Booting and printing 'Hello World'

--- bootloader/src/start.asm
+++ bootloader/src/start.asm
@@ -3,8 +3,39 @@
 
 entry:
     mov ah, 0x0e
-    mov al, 'h'
+    mov al, 'H'
     int 0x10
+
+    mov al, 'e'
+    int 0x10
+
+    mov al, 'l'
+    int 0x10
+
+    mov al, 'l'
+    int 0x10
+
+    mov al, 'o'
+    int 0x10
+
+    mov al, ' '
+    int 0x10
+
+    mov al, 'W'
+    int 0x10
+
+    mov al, 'o'
+    int 0x10
+
+    mov al, 'r'
+    int 0x10
+
+    mov al, 'l'
+    int 0x10
+
+    mov al, 'd'
+    int 0x10
+
     jmp $
 
 times 510 - ($ - $$) db 0
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::process::Command;
 fn main() {
-    println!("Hello, world!");
+    // TODO(patrik): Build the bootloader
+      // TODO(patrik): Create a PE Parser
+      // TODO(patrik): Flatten the PE
+    // TODO(patrik): Assemble start.asm
+
+    std::fs::create_dir_all("build")
+        .expect("Failed to create the build directory");
+
+    println!("Assembling 'start.asm'");
+    let output = Command::new("nasm")
+                    .args(&[
+                        "-f", "bin", 
+                        "bootloader/src/start.asm", 
+                        "-o", "build/start.bin"])
+                    .output()
+                    .expect("Failed to assemble 'start.asm'");
+    println!("{:?}", output.stdout);
 }
