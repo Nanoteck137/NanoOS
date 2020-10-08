c21c45929b0155be0e02a4bb4d2b0bf69951b01f
Nanoteck137 <patrik.millvik@gmail.com>
2020-10-08 23:48:29 +0200

    Working on the file system

--- test/fat32/src/main.rs
+++ test/fat32/src/main.rs
@@ -55,114 +55,286 @@ fn parse_partition_entry(bytes: &[u8]) -> Option<PartitionEntry> {
     });
 }
 
-fn parse_extended_bpb_32(bytes: &[u8]) -> Option<()> {
+#[derive(Debug)]
+struct ExtendedBPB32 {
+    fat_size: u32,
+    flags: u16,
+    fs_version: u16,
+    root_cluster: u32,
+    fs_info: u16,
+    backup_boot_sector: u16,
+    drive_number: u8,
+    boot_signature: u8,
+    volume_id: u32,
+    volume_label: [u8; 11],
+    fs_type: [u8; 8]
+}
+
+fn parse_extended_bpb_32(bytes: &[u8]) -> Option<ExtendedBPB32> {
     if bytes.len() > 447 {
         return None;
     }
 
     let fat_size = 
         u32::from_le_bytes(bytes[0..4].try_into().ok()?);
-    println!("Fat Size: {}", fat_size);
 
     let flags = 
         u16::from_le_bytes(bytes[4..6].try_into().ok()?);
-    println!("Flags: {}", flags);
 
     let fs_version = 
         u16::from_le_bytes(bytes[6..8].try_into().ok()?);
-    println!("FS Version: {}", fs_version);
 
     let root_cluster = 
         u32::from_le_bytes(bytes[8..12].try_into().ok()?);
-    println!("Root Cluster: {}", root_cluster);
 
     let fs_info = 
         u16::from_le_bytes(bytes[12..14].try_into().ok()?);
-    println!("FS Info: {}", fs_info);
     
     let backup_boot_sector =
         u16::from_le_bytes(bytes[14..16].try_into().ok()?);
-    println!("Backup boot sector: {}", backup_boot_sector);
 
-    let reserved: [u8; 12] = bytes[16..28].try_into().ok()?;
+    // let reserved: [u8; 12] = bytes[16..28].try_into().ok()?;
 
     let drive_number = bytes[28];
-    println!("Drive Number: {:#x}", drive_number);
 
-    let reserved2 = bytes[29];
+    // let reserved2 = bytes[29];
 
     let boot_signature = bytes[30];
-    println!("Boot Signature: {:#x}", boot_signature);
 
     let volume_id = 
         u32::from_le_bytes(bytes[31..35].try_into().ok()?);
-    println!("Volume ID: {:#x}", volume_id);
 
     let volume_label: [u8; 11] = bytes[35..46].try_into().ok()?;
-    println!("Volume Label: {}", std::str::from_utf8(&volume_label).unwrap());
 
     let fs_type: [u8; 8] = bytes[46..54].try_into().ok()?;
-    println!("FS Type: {}", std::str::from_utf8(&fs_type).unwrap());
 
-    Some(())
+    Some(ExtendedBPB32 {
+        fat_size,
+        flags,
+        fs_version,
+        root_cluster,
+        fs_info,
+        backup_boot_sector,
+        drive_number,
+        boot_signature,
+        volume_id,
+        volume_label,
+        fs_type
+    })
 }
 
-fn parse_bpb(bytes: &[u8]) -> Option<()> {
+#[derive(Debug)]
+struct BPB {
+    jmp: [u8; 3],
+    oem_name: [u8; 8],
+    bytes_per_sector: u16,
+    sectors_per_cluster: u8,
+    reserved_sector_count: u16,
+    num_fats: u8,
+    root_entry_count: u16,
+    total_sectors_16: u16,
+    media: u8,
+    fat_size_16: u16,
+    sectors_per_track: u16,
+    number_of_heads: u16,
+    hidden_sectors: u32,
+    total_sectors_32: u32,
+}
+
+fn parse_bpb(bytes: &[u8]) -> Option<(BPB, ExtendedBPB32)> {
     if bytes.len() > 512 {
         return None;
     }
 
     let jmp: [u8; 3] = bytes[0..3].try_into().ok()?;
-    println!("Jump: {:x?}", jmp);
 
     let oem_name: [u8; 8] = bytes[3..11].try_into().ok()?;
-    println!("OEM Name: {:?}", std::str::from_utf8(&oem_name).ok()?);
 
     let bytes_per_sector = 
         u16::from_le_bytes(bytes[11..13].try_into().ok()?);
-    println!("Bytes per sector: {}", bytes_per_sector);
 
     let sectors_per_cluster: u8 = bytes[13];
-    println!("Sectors per cluster: {}", sectors_per_cluster);
 
     let reserved_sector_count = 
         u16::from_le_bytes(bytes[14..16].try_into().ok()?);
-    println!("Reserved sector count: {}", reserved_sector_count);
 
     let num_fats = bytes[16];
-    println!("Num fats: {}", num_fats);
 
     let root_entry_count = u16::from_le_bytes(bytes[17..19].try_into().ok()?);
-    println!("Root entry count: {}", root_entry_count);
 
     let total_sectors_16 = 
         u16::from_le_bytes(bytes[19..21].try_into().ok()?);
-    println!("Total sectors 16: {}", total_sectors_16);
 
     let media = bytes[21];
-    println!("Media: {}", media);
 
     let fat_size_16 = 
         u16::from_le_bytes(bytes[22..24].try_into().ok()?);
-    println!("Fat size 16: {}", fat_size_16);
 
     let sectors_per_track = 
         u16::from_le_bytes(bytes[24..26].try_into().ok()?);
-    println!("Sectors per track: {}", sectors_per_track);
     
     let number_of_heads = 
         u16::from_le_bytes(bytes[26..28].try_into().ok()?);
-    println!("Number of heads: {}", number_of_heads);
 
     let hidden_sectors = 
         u32::from_le_bytes(bytes[28..32].try_into().ok()?);
-    println!("Number of hidden sectors: {}", hidden_sectors);
 
     let total_sectors_32 = 
         u32::from_le_bytes(bytes[32..36].try_into().ok()?);
-    println!("Total sectors 32: {}", total_sectors_32);
 
-    parse_extended_bpb_32(&bytes[36..448])?;
+    let extended_bpb_32 = parse_extended_bpb_32(&bytes[36..448])?;
+
+    Some((BPB {
+        jmp,
+        oem_name,
+        bytes_per_sector,
+        sectors_per_cluster,
+        reserved_sector_count,
+        num_fats,
+        root_entry_count,
+        total_sectors_16,
+        media,
+        fat_size_16,
+        sectors_per_track,
+        number_of_heads,
+        hidden_sectors,
+        total_sectors_32,
+    }, extended_bpb_32))
+}
+
+fn parse_directory_entry(bytes: &[u8]) -> Option<()> {
+    let name: [u8; 8] = bytes[0..8].try_into().ok()?;
+    println!("Name: {}", std::str::from_utf8(&name).unwrap());
+    let ext: [u8; 3] = bytes[8..11].try_into().ok()?;
+    println!("Ext: {}", std::str::from_utf8(&ext).unwrap());
+    let attributes = bytes[11];
+    println!("Attributes: {:#x}", attributes);
+    
+    let reserved = bytes[12];
+    println!("Reserved: {}", reserved);
+
+    let undelete = bytes[13];
+    println!("Undelete: {}", undelete);
+
+    let creation_time = 
+        u16::from_le_bytes(bytes[14..16].try_into().ok()?);
+    println!("Creation time: {:#x}", creation_time);
+
+    let creation_date =
+        u16::from_le_bytes(bytes[16..18].try_into().ok()?);
+    println!("Creation date: {:#x}", creation_date);
+
+    let last_accessed_date =
+        u16::from_le_bytes(bytes[18..20].try_into().ok()?);
+    println!("Last accessed date: {:#x}", last_accessed_date);
+
+    let cluster_high = 
+        u16::from_le_bytes(bytes[20..22].try_into().ok()?);
+    println!("Cluster High: {:#x}", cluster_high);
+
+    let last_modification_time =
+        u16::from_le_bytes(bytes[22..24].try_into().ok()?);
+    println!("Last modification time: {:#x}", last_modification_time);
+
+    let last_modification_date =
+        u16::from_le_bytes(bytes[24..26].try_into().ok()?);
+    println!("Last modification date: {:#x}", last_modification_date);
+
+    let cluster_low =
+        u16::from_le_bytes(bytes[26..28].try_into().ok()?);
+    println!("Cluster low: {:#x}", cluster_low);
+
+    let cluster = ((cluster_high as u32) << 16) | cluster_low as u32;
+    println!("Cluster: {:#x}", cluster);
+
+    let file_size = 
+        u32::from_le_bytes(bytes[28..32].try_into().ok()?);
+    println!("File Size: {}", file_size);
+
+
+    Some(())
+}
+
+#[derive(Debug)]
+struct LFNEntry {
+    entry_order: u8,
+    name: [u16; 13],
+    attribute: u8,
+    long_entry_type: u8,
+    checksum: u8,
+}
+
+fn parse_lfn_entry(bytes: &[u8]) -> Option<LFNEntry> {
+    let entry_order = bytes[0];
+    let name_first: [u8; 10] = bytes[1..11].try_into().ok()?;
+
+    let attribute = bytes[11];
+
+    let long_entry_type = bytes[12];
+
+    let checksum = bytes[13];
+
+    let name_middle: [u8; 12] = bytes[14..26].try_into().ok()?;
+
+    let name_last: [u8; 4] = bytes[28..32].try_into().ok()?;
+
+    let mut name_bytes: [u8; 26] = [0u8; 26];
+    name_bytes[0..10].copy_from_slice(&name_first);
+    name_bytes[10..22].copy_from_slice(&name_middle);
+    name_bytes[22..26].copy_from_slice(&name_last);
+
+    let mut name: [u16; 13] = [0u16; 13];
+    for index in 0..13 {
+        name[index] = 
+            u16::from_le_bytes(name_bytes[index * 2..index * 2 + 2]
+                               .try_into().ok()?);
+    }
+
+    Some(LFNEntry {
+        entry_order,
+        name,
+        attribute,
+        long_entry_type,
+        checksum,
+    })
+}
+
+fn parse_directory(bytes: &[u8]) -> Option<()> {
+    let mut file_name: [u8; 255] = [0u8; 255];
+    for index in 0..16 {
+        let offset = index * 32;
+        if bytes[offset] == 0 { break; }
+        if bytes[offset] == 0xE5 { continue; }
+
+        if bytes[offset + 11] == 0xf { 
+            let lfn_entry = parse_lfn_entry(&bytes[offset..offset+32])?; 
+
+            if (lfn_entry.entry_order & 0x40) != 0 {
+                let order = lfn_entry.entry_order & 0xf; 
+                let offset = order * 13;
+                for index in 0..13 {
+                    let character = lfn_entry.name[index] & 0xff;
+                    if character != 0xff {
+                        file_name[index + offset as usize] = 
+                            character as u8;
+                    }
+                }
+            } else {
+                let order = lfn_entry.entry_order & 0xf; 
+                let offset = order * 13;
+                for index in 0..13 {
+                    file_name[index + offset as usize] = 
+                        (lfn_entry.name[index] & 0xff) as u8;
+                }
+            }
+        } else {
+            println!("File: {}", std::str::from_utf8(&file_name).unwrap());
+            parse_directory_entry(&bytes[offset..offset+32])?;
+            file_name = [0u8; 255];
+            println!();
+        }
+
+    }
 
     Some(())
 }
@@ -202,7 +374,32 @@ fn main() -> Result<(), Box<dyn Error>> {
     // disk image so for now we can select the first entry
     let fat_partition_entry = parition_entries[0];
     let partition_offset = fat_partition_entry.lba_address as usize * 512;
-    parse_bpb(&bytes[partition_offset..partition_offset + 512]).unwrap();
+    let (bpb, extended_bpb_32) = 
+        parse_bpb(&bytes[partition_offset..partition_offset + 512]).unwrap();
+    println!("BPB: {:#?}", bpb);
+    println!("Extended BPB: {:#?}", extended_bpb_32);
+
+    let root_dir_sectors = 
+        ((bpb.root_entry_count * 32) + (bpb.bytes_per_sector - 1)) / bpb.bytes_per_sector;
+    println!("Root Dir Sectors: {}", root_dir_sectors);
+
+    let root_dir_sectors = root_dir_sectors as u32;
+    let reserved_sector_count = bpb.reserved_sector_count as u32;
+    let num_fats = bpb.num_fats as u32;
+    let fat_size = extended_bpb_32.fat_size;
+    let first_data_sector = 
+        reserved_sector_count + (num_fats * fat_size) + root_dir_sectors;
+    println!("First data sector: {}", first_data_sector);
+
+    let cluster_number = extended_bpb_32.root_cluster;
+    let sectors_per_cluster = bpb.sectors_per_cluster as u32;
+    let first_sector_of_cluster = 
+        ((cluster_number - 2) * sectors_per_cluster) + first_data_sector;
+    println!("First Sector of cluster 2: {}", first_sector_of_cluster);
+
+    let offset: usize = 
+        ((first_sector_of_cluster + fat_partition_entry.lba_address) * 512).try_into()?;
+    parse_directory(&bytes[offset..offset+512]).unwrap();
 
     Ok(())
 }
