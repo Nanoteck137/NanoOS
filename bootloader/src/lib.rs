c2e8495ee20a13460ac78e140919bb3af8d2d643
Patrik M. Rosenström <patrik.millvik@gmail.com>
2020-10-11 01:28:37 +0200

    Added comments

--- bootloader/src/lib.rs
+++ bootloader/src/lib.rs
@@ -29,29 +29,39 @@ extern fn entry() -> ! {
 
     println!("Welcome to NanoOS Bootloader v0.1");
 
+    // Initialize an empty range set
     let mut range_set = RangeSet::new();
 
     unsafe {
+        // Setup a pointer to the first memory map entry located at 0x0500, 
+        // this address is specified in the stage0 code
         let entry_ptr = 0x0500 as *const MemoryMapEntry;
+        // Get the first entry
         let mut entry = entry_ptr.offset(0);
         loop {
+            // Dereference the pointer
             let e = *entry;
+            // Check if this entry is the last, the last entry has the typ = 0
             if e.typ == 0 {
                 break;
             }
 
-            // println!("Entry: {:#x?}", e);
+            // typ = 1 is usable memory so add it to the range set
             if e.typ == 1 {
+                // Insert the range
                 range_set.insert(Range { 
                     start: e.address, 
                     end: e.address.checked_add(e.length - 1).unwrap()
                 });
             }
 
+            // Get the next entry
             entry = entry.offset(1);
         }
     }
 
+    // Remove the first 1 MiB because it has alot of memory map 
+    // that could be usefull
     range_set.remove(Range {
         start: 0,
         end: 1 * 1024 * 1024 - 1
